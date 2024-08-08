cd264356cda2e078b6d2f1377dff0974498eaa3c
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-08-08 15:26:18 +0530

    avrahe print

--- Cpp/addtion.cpp
+++ Cpp/addtion.cpp
@@ -1,14 +1,32 @@
-#include<iostream>;
+#include<iostream>
 using namespace std;
 
 int main(){
-    int a,b,c;
+    // int a,b,c;
 
-    cout<<"Enter the Frist Number: "<<endl;
-    cin>>a;
-    cout<<"Enter the secound Number: "<<endl;
-    cin>>b;
+    // cout<<"Enter the Frist Number: "<<endl;
+    // cin>>a;
+    // cout<<"Enter the secound Number: "<<endl;
+    // cin>>b;
 
-    c = a + b;
-    cout<<"Addition is : "<<c<<endl;
+    // c = a + b;
+    // cout<<"Addition is : "<<c<<endl;
+
+
+    // print average marks
+
+    float science , maths, english, avg;
+
+    cout<<"Enter Science marks :";
+    cin>>science;
+    cout<<"Enter maths marks :";
+    cin>>maths;
+    cout<<"Enter english marks :";
+    cin>>english;
+
+
+    avg = (science + english + maths) / 3;
+
+    cout<<"Your avrage is: "<<avg;
+
 }
