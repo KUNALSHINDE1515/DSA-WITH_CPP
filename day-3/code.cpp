0b0cb412fe3cb59ad74954644efcc646853bcb00
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-08-09 11:51:09 +0530

    typecasting in cpp

---
+++ day-3/code.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+//symbolic constant
+#define ll long long;
+using namespace std;
+
+int main(){
+
+    const int num = 25;
+    int num2 = 25;
+    num2 = 35;
+    // num = 95 // not allowed
+
+    const float PI = 3.14;
+    // const float G ;
+    // g = 25;  // not allowed
+
+    
+    return 0;
+}
